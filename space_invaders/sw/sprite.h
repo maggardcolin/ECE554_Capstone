2857c16861fc3ad4c5657284f4b09d8bc10e65a6
Colin Maggard <maggardcolin@gmail.com>
2026-02-03 21:46:09 -0600

    move sprites

--- space_invaders/sw/sprite.h
+++ space_invaders/sw/sprite.h
@@ -1,6 +1,80 @@
 #pragma once
 #include <stdint.h>
 
+// Sprite ASCII art definitions (row-packed)
+static const char *player_rows[] = {
+    "....XX....",
+    "...XXXX...",
+    "..XXXXXX..",
+    ".XXXXXXXX.",
+    "XXXXXXXXXX",
+    "XX..XX..XX",
+    "XX..XX..XX",
+    "..........",
+};
+static const char *alienA_rows[] = {
+    "..XX...XX..",
+    "...XXXXX...",
+    "..XXXXXXX..",
+    ".XX.XXX.XX.",
+    "XXXXXXXXXXX",
+    "..X.....X..",
+    ".X.......X.",
+    "X.........X",
+};
+static const char *alienB_rows[] = {
+    "..XX...XX..",
+    "X..XXXXX..X",
+    "X.XXXXXXX.X",
+    "XX.XXX.XXX.",
+    "XXXXXXXXXXX",
+    ".X..X.X..X.",
+    "X..X...X..X",
+    ".XX.....XX.",
+};
+static const char *bossA_rows[] = {
+    "....XXXX....XXXX....XXXX....",
+    "..XXXXXXXX..XXXX..XXXXXXXX..",
+    ".XXXX..XXXXXXXXXXXX..XXXX...",
+    "XXXX..XXXXXXXXXXXXXX..XXXX..",
+    "XXXXXX..XXXXXXXXXX..XXXXXX..",
+    "XXXXXXXX..XXXXXX..XXXXXXXX..",
+    "XXXX..XXXXXXXXXXXXXX..XXXX..",
+    "..XXXX..XXXXXXXXXX..XXXX....",
+    "....XXXXXXXX..XXXXXXXX......",
+    "......XX..XX..XX..XX........",
+};
+static const char *bossB_rows[] = {
+    "....XXXX....XXXX....XXXX....",
+    "..XXXXXXXX..XXXX..XXXXXXXX..",
+    ".XXXX..XXXXXXXXXXXX..XXXX...",
+    "XXXX..XXXXXXXXXXXXXX..XXXX..",
+    "XXXXXX..XXXXXXXXXX..XXXXXX..",
+    "XXXXXXXX..XXXXXX..XXXXXXXX..",
+    "XXXX..XXXXXXXXXXXXXX..XXXX..",
+    "..XXXX..XXXXXXXXXX..XXXX....",
+    "....XXXXXXXX..XXXXXXXX......",
+    "........XX..XX..XX..XX......",
+};
+static const char *bunker_rows[] = {
+    "....XXXXXXXXXXXX....",
+    "...XXXXXXXXXXXXXX...",
+    "..XXXXXXXXXXXXXXXX..",
+    ".XXXXXXXXXXXXXXXXXX.",
+    "XXXXXXXXXXXXXXXXXXXX",
+    "XXXXXXX......XXXXXXX",
+    "XXXXXX........XXXXXX",
+    "XXXXX..........XXXXX",
+    "XXXXX..........XXXXX",
+    "XXXXX..........XXXXX",
+    "XXXXX..........XXXXX",
+    "XXXXX..........XXXXX",
+    "XXXXX..........XXXXX",
+    "XXXXX..........XXXXX",
+    "....................",
+    "....................",
+};
+
 typedef struct {
     int w, h;
     int stride;    // bytes per row
