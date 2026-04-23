1699bd0762087388e6c750a11cc896c01d3765ee
maggardcolin <maggardcolin@gmail.com>
2026-04-23 12:51:11 -0500

    clean up repo

--- fighting_game/sw/font.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include "gfx.h"
-
-void l_draw_digit(lfb_t *lfb, int x0, int y0, int d, uint32_t c);
-void l_draw_score(lfb_t *lfb, int x, int y, int score, uint32_t c);
-void l_draw_text(lfb_t *lfb, int x, int y, const char *text, int scale, uint32_t c);
